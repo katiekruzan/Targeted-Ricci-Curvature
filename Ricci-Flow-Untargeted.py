7a326c58c55937bed5a183bb2838d1d58fd2aa99
Katie Kruzan <katiekruzan@gmail.com>
2025-07-30 11:35:32 -0500

    update the default approx version

--- Ricci-Flow-Untargeted.py
+++ Ricci-Flow-Untargeted.py
@@ -515,6 +515,6 @@ if __name__ == "__main__":
     maximum_error = True # False is average error
     approx_emd = os.environ.get('APPROX') 
     if approx_emd is None: # Make the default False
-        approx_emd = True
+        approx_emd = False
     
     main()
