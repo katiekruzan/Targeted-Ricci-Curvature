1645fb2a3faf42c48f26a1a47a4dfdda4590b44d
Katie Kruzan <katiekruzan@gmail.com>
2025-11-05 17:16:41 -0600

    rename

--- targeted-parallel.py
+++ targeted-parallel.py
@@ -3,6 +3,7 @@ mpiexec -n 4 python .\tests-parallel.py
 '''
   
 from hypergraph import *
+from ricciutil import *
 import os
 from mpi4py import MPI
 import time
@@ -10,67 +11,6 @@ import pandas as pd
 import csv
 
 COMM = MPI.COMM_WORLD
-
-# helper functions
-def clean_output(verbose:bool) -> None:
-    '''puts all the files (other than the README) in the outputfiles/ folder into a subfolder
-
-    :param bool verbose: Can turn on to print the file names it has moved.
-    '''
-    files = os.listdir('outputfiles')
-    now = time.time()
-    if not os.path.isdir(f'outputfiles/{now}'):
-        os.makedirs(f'outputfiles/{now}')
-    for f in files:
-        if f=='README.md' or os.path.isdir(f'outputfiles/{f}'):
-            continue
-        else:
-            try:
-                os.rename(f'outputfiles/{f}', f'outputfiles/{now}/{f}')
-                if verbose:
-                    print(f'moving {f} to {now}/{f}')
-            except:
-                print(f'Had issues moving {f} to a new folder')
-    return
-
-def write_scorecard(line:str)-> None:
-  '''function used to write the key results to a scorecard (for posterity)
-
-  :param str line: text to be put onto the scorecard
-  '''    
-  with open('outputfiles/scorecard.txt', 'a+') as f:
-      f.write(line)
-      f.write('\n')
-      
-def clock_time(message:str)-> None:
-    '''Get the time from the start of the process and write it to the scorecard with some message
-
-    :param str message: The message to put before the time being spent
-    '''
-    now = time.time()
-    rt = now-start
-    write_scorecard(f'{message}: {rt}')
-    return
-  
-def save_matrix_csv(matrix:list[list], filename:str) -> None:
-  '''Function to save the matrix as a CSV file
-
-  :param list[list] matrix: matrix to be written
-  :param str filename: place to write it
-  '''    
-  pd.DataFrame(matrix).to_csv(filename, index=False, header=False)
-  return
-  
-def ricci_normalizing(R: float)->float: 
-  '''
-  Using the normalization function sigma(R)/sigma(1) 
-  Where sigma(x) is the standard sigmoid function 1/(1+\exp(-x))
-
-  :param float R: the ORC value to be normalized 
-  :return float: The normalized ORC value
-  ''' 
-  # return (1/(1+ np.exp(-R)))
-  return (1/(1+ np.exp(-R)))
 
 # process functions
 def early_analysis(src_graph:Hypergraph, verbose:bool):
@@ -164,7 +104,6 @@ def set_up_one_direction(src_graph:Hypergraph, targ_graph:Hypergraph, op_flag=Fa
 
 
 # Manager Functions
-
 def calculate_target_orc_manager(npr:int, distance_matrix: list[list], graph:Hypergraph, verbose:bool, op_flag=False):
   '''The function to calculate the staring info for the target graph
 
@@ -561,6 +500,8 @@ if __name__ == "__main__":
     quit()
   
   start = time.time()
+  # moves this to the util
+  set_start(start)
   
   RANK = COMM.Get_rank()
   SIZE = COMM.Get_size()
